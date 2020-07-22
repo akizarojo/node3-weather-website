7c39c2ba588690e661bda863bf9337b3c4219c84
Akiza Rojo <akizadyanrojo@gmail.com>
2020-07-22 22:16:47 +0800

    Add weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('postman-request')
 
 const forecast = (latitude,longtitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1e6d402c19c7ee366eb246dd2c4ebf32&query=' + encodeURIComponent(longtitude) + ',' + encodeURIComponent(latitude) +'&units=s'
-        
+        console.log(url)
     request({url, json: true},(error, {body} = {}) => {
         if(error){
             callback('Unable to connect to the internet',undefined)
@@ -14,7 +14,7 @@ const forecast = (latitude,longtitude, callback) => {
                 const temperature = body.current.temperature
                 const feelsLike = body.current.feelslike
                 const weather = body.current.weather_descriptions[0]
-                const data = weather + ". It is currently " + temperature + " degress out. But it feels like " + feelsLike + " degress out"
+                const data = weather + ". It is currently " + temperature + " degress out. But it feels like " + feelsLike + " degress out, and it will going to be " + weather
                 callback(undefined,data)
         }
 
